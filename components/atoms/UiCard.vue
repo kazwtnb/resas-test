3e32546d8d6261bf3cb7c3ed8a6f176f57c74c13
Kazuki Watanabe <kazuki.watanabe@bizreach.co.jp>
2022-05-07 22:53:37 +0900

    feat: CardのUIコンポーネントを作成して配置

---
+++ components/atoms/UiCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="ui-card">
+    <slot />
+  </div>
+</template>
+<script setup lang="ts">
+interface Props {
+  width?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  width: '100%'
+});
+</script>
+<style lang="scss" scoped>
+.ui-card {
+  padding: 1.5rem;
+  width: v-bind(width);
+  border-radius: 4px;
+  background-color: colors.$color-white;
+  box-shadow: colors.$shadow-1;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,16 @@
 <template>
   <AtomsUiSection>
     <h1>都道府県別人口構成</h1>
+    <AtomsUiCard>
+      <p>hogehoge</p>
+    </AtomsUiCard>
   </AtomsUiSection>
 </template>
 <script setup lang="ts"></script>
 <style lang="scss" scoped>
 h1 {
   text-align: center;
+
   + * {
     margin-top: 20px;
   }
